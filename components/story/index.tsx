51c7125995232a81f40a9df813fddf47424c91f9
Pratik Unhale <pratik.unhale@coditas.com>
2023-04-06 12:10:48 +0530

    story style update

--- components/story/index.tsx
+++ components/story/index.tsx
@@ -23,69 +23,71 @@ const Story: React.FC<IStoryProps> = ({ stories, game, selectedStory, handleStor
         router.push(`/dashboard`);
     };
 
+    const storiesExist = stories?.length as number > 0
 
-    return (<div className={styles["container"]}>
-        {stories?.length as number > 0 ?
-            <div className={styles["title"]}>
-                <p>Story</p>
-                <p>Score</p>
+    return (
+        <div className={storiesExist ? [styles["container"], styles["bgWhite"]].join(" ") : styles["container"]}>
+            {storiesExist ?
+                <div className={styles["title"]}>
+                    <p>Story</p>
+                    <p>Score</p>
+                </div>
+                : null}
+            <div className={styles["storyList"]}>
+                {
+                    stories?.map((story) => {
+                        const isSelected = selectedStory === story
+                        return (
+                            <div
+                                className={isSelected ? styles["storySelected"] : styles["story"]}
+                                key={story.id}
+                                onClick={() => handleStorySelect(story)}
+                            >
+                                <div className={styles["storyName"]}>{story.name}</div>
+                                <div className={styles["storyAverage"]}>{story.average || '-/-'}</div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
-            : null}
-        <div className={styles["storyList"]}>
             {
-                stories?.map((story) => {
-                    const isSelected = selectedStory === story
-                    return (
+                isModerator(game.createdById, currentPlayerId) ? <>
+                    <div className={styles["addStoryContainer"]} >
                         <div
-                            className={isSelected ? styles["storySelected"] : styles["story"]}
-                            key={story.id}
-                            onClick={() => handleStorySelect(story)}
-                        >
-                            <div className={styles["storyName"]}>{story.name}</div>
-                            <div className={styles["storyAverage"]}>{story.average || '-/-'}</div>
-                        </div>
-                    )
-                })
+                            className={styles["addStory"]}
+                            onClick={() => setIsAddStoryModalOpen(!isAddStoryModalOpen)}
+                        >+ Add Story</div>
+                    </div>
+                    <div className={styles["footer"]} >
+                        <div
+                            className={styles["endGame"]}
+                            onClick={leaveGame}
+                        >  X End Game</div>
+                    </div>
+                </>
+                    : null
             }
 
-        </div>
-        {
-            isModerator(game.createdById, currentPlayerId) ? <>
-                <div className={styles["addStoryContainer"]} >
-                    <div
-                        className={styles["addStory"]}
-                        onClick={() => setIsAddStoryModalOpen(!isAddStoryModalOpen)}
-                    >+ Add Story</div>
-                </div>
-                <div className={styles["footer"]} >
-                    <div
-                        className={styles["endGame"]}
-                        onClick={leaveGame}
-                    >  X End Game</div>
-                </div>
-            </>
-                : null
-        }
 
-
-        {
-            isAddStoryModalOpen ? <AddStoryModal
-                gameId={game.id}
-                handleCloseModal={() => setIsAddStoryModalOpen(false)}
-                players={players}
-            ></AddStoryModal> : null
-        }
-        {
-            isDeleteStoryModalOpen && selectedStory ?
-                <DeleteModal
-                    closeModalCallBack={() => setIsDeleteStoryModalOpen(false)}
-                    title={`Delete ${selectedStory.name}`}
-                    handleDeleteCallback={
-                        () => removeStory(game.id, selectedStory.id)
-                    }
-                /> : null
-        }
-    </div>)
+            {
+                isAddStoryModalOpen ? <AddStoryModal
+                    gameId={game.id}
+                    handleCloseModal={() => setIsAddStoryModalOpen(false)}
+                    players={players}
+                ></AddStoryModal> : null
+            }
+            {
+                isDeleteStoryModalOpen && selectedStory ?
+                    <DeleteModal
+                        closeModalCallBack={() => setIsDeleteStoryModalOpen(false)}
+                        title={`Delete ${selectedStory.name}`}
+                        handleDeleteCallback={
+                            () => removeStory(game.id, selectedStory.id)
+                        }
+                    /> : null
+            }
+        </div>)
 }
 
 export default Story
